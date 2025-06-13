563b5fca1d162acb4f0a28eccb8d12852bb8ef1a
Kresh <srikresh11@gmail.com>
2025-06-13 23:50:40 +0530

    port fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.static("public"));
 
